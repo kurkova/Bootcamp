852ecce4e85513c0b6361e8e70e2820c32b0d100
kurkova <kurkiewiczpaula@gmail.com>
2018-11-09 11:42:55 +0000

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+           Calculator calculator = new Calculator();
+
+           int a = 8;
+           int b = 6;
+
+           int addResult = calculator.addAtoB(a,b);
+           if (addResult == 14){
+               System.out.println("Method of adding test ok");
+           }else{
+               System.out.println("Method of adding has Error");
+           }
+
+           int substractResult = calculator.substractAfromB(a,b);
+           if (substractResult == 2){
+               System.out.println("Method of substraction test Ok");
+           }else{
+               System.out.println("Method of substraction has error");
+           }
     }
 }
