3de162d047d24feec66f17d36474b5a5d49699f7
kurkova <kurkiewiczpaula@gmail.com>
2019-01-09 12:40:52 +0000

    first commit

--- kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
@@ -50,7 +50,7 @@ public final class Bigmac {
         }
     }
 
-    public Bigmac(final String bun, final int burgers, final String sauce, final List<String> ingredients) {
+    private Bigmac(final String bun, final int burgers, final String sauce, final List<String> ingredients) {
         this.bun = bun;
         this.burgers = burgers;
         this.sauce = sauce;
