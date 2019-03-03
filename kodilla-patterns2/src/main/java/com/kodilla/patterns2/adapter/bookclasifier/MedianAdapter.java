05a707100919749082a84a9242936746fb0e884b
kurkova <kurkiewiczpaula@gmail.com>
2019-03-03 15:29:46 +0000

    Corected task 20.5

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
@@ -1,6 +1,5 @@
 package com.kodilla.patterns2.adapter.bookclasifier;
 
-import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
 import com.kodilla.patterns2.adapter.bookclasifier.librarya.Classifier;
 import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
 
@@ -10,7 +9,7 @@ import java.util.Set;
 
 public class MedianAdapter extends MedianAdaptee implements Classifier {
     @Override
-    public int publicationYearMedian (Set<com.kodilla.patterns2.adapter.bookclasifier.librarya.Book> bookSet) {
+    public int publicationYearMedian(Set<com.kodilla.patterns2.adapter.bookclasifier.librarya.Book> bookSet) {
         Map<BookSignature, com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book> bookMap =
                 new HashMap<>();
         for (com.kodilla.patterns2.adapter.bookclasifier.librarya.Book book : bookSet) {
--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -3,8 +3,10 @@ package com.kodilla.patterns2.adapter.bookclassifier;
 import com.kodilla.patterns2.adapter.bookclasifier.MedianAdapter;
 import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
 import org.junit.Test;
+
 import java.util.HashSet;
 import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 
 public class MedianAdapterTestSuite {
@@ -12,30 +14,30 @@ public class MedianAdapterTestSuite {
     public void publicationYearMedianTest() {
         //Given
         MedianAdapter medianAdapter = new MedianAdapter();
-        Set<Book> bookA = new HashSet<>();
-        bookA.add(new Book("Rowling",
+        Set<Book> booksFromLibraryA = new HashSet<>();
+        booksFromLibraryA.add(new Book("Rowling",
                 "Harry Potter I",
                 2000,
                 "AAA123"));
-        bookA.add(new Book("Rowling",
+        booksFromLibraryA.add(new Book("Rowling",
                 "Harry Potter II",
                 2002,
                 "AAA124"));
-        bookA.add(new Book("Rowling",
+        booksFromLibraryA.add(new Book("Rowling",
                 "Harry Potter III",
                 2004,
                 "AAA125"));
-        bookA.add(new Book("Rowling",
+        booksFromLibraryA.add(new Book("Rowling",
                 "Harry Potter IV",
                 2006,
                 "AAA126"));
 
-        bookA.add(new Book("Rowling",
+        booksFromLibraryA.add(new Book("Rowling",
                 "Harry Potter V",
                 2008,
                 "AAA127"));
         //When
-        int median = medianAdapter.publicationYearMedian(bookA);
+        int median = medianAdapter.publicationYearMedian(booksFromLibraryA);
         //Then
         assertEquals(2004, median);
     }
