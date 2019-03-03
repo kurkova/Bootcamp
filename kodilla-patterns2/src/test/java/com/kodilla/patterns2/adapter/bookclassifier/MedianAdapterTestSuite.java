29013a766256bb861ff2d823417d2b5b2a333581
kurkova <kurkiewiczpaula@gmail.com>
2019-03-03 15:35:59 +0000

    Corrected task 20.5

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -14,30 +14,30 @@ public class MedianAdapterTestSuite {
     public void publicationYearMedianTest() {
         //Given
         MedianAdapter medianAdapter = new MedianAdapter();
-        Set<Book> books = new HashSet<>();
-        books.add(new Book("Rowling",
+        Set<Book> booksFromLibraryA = new HashSet<>();
+        booksFromLibraryA.add(new Book("Rowling",
                 "Harry Potter I",
                 2000,
                 "AAA123"));
-        books.add(new Book("Rowling",
+        booksFromLibraryA.add(new Book("Rowling",
                 "Harry Potter II",
                 2002,
                 "AAA124"));
-        books.add(new Book("Rowling",
+        booksFromLibraryA.add(new Book("Rowling",
                 "Harry Potter III",
                 2004,
                 "AAA125"));
-        books.add(new Book("Rowling",
+        booksFromLibraryA.add(new Book("Rowling",
                 "Harry Potter IV",
                 2006,
                 "AAA126"));
 
-        books.add(new Book("Rowling",
+        booksFromLibraryA.add(new Book("Rowling",
                 "Harry Potter V",
                 2008,
                 "AAA127"));
         //When
-        int median = medianAdapter.publicationYearMedian(books);
+        int median = medianAdapter.publicationYearMedian(booksFromLibraryA);
         //Then
         assertEquals(2004, median);
     }
