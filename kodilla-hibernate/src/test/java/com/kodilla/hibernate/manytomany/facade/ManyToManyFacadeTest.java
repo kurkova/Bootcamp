f673e5c51675a335bd73764ef7d42e0836b712b8
kurkova <kurkiewiczpaula@gmail.com>
2019-03-27 19:17:50 +0000

    Hubernate

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/ManyToManyFacadeTest.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/ManyToManyFacadeTest.java
@@ -1,103 +1,103 @@
-//package com.kodilla.hibernate.manytomany.facade;
-//
-//import com.kodilla.hibernate.manytomany.Company;
-//import com.kodilla.hibernate.manytomany.Employee;
-//import com.kodilla.hibernate.manytomany.dao.CompanyDao;
-//import com.kodilla.hibernate.manytomany.dao.EmployeeDao;
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//
-//import java.util.List;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//@Transactional
-//public class ManyToManyFacadeTest {
-//    @Autowired
-//    private ManyToManyFacade manyToManyFacade;
-//    @Autowired
-//    private CompanyDao companyDao;
-//    @Autowired
-//    private EmployeeDao employeeDao;
-//
-//    @Test
-//    public void findEmployeeNameLikeTest() {
-//        //Given
-//        Employee paulaWalk = new Employee("Paul", "Walk");
-//        Employee johnStaff = new Employee("John", "Staff");
-//        Employee donaldBret = new Employee("Donald", "Bret");
-//
-//        Company softwareApp = new Company("Software App");
-//        Company softwareLen = new Company("Software len");
-//        Company appleSoft = new Company("Apple Soft");
-//
-//        softwareApp.getEmployees().add(paulaWalk);
-//        softwareLen.getEmployees().add(johnStaff);
-//        softwareLen.getEmployees().add(donaldBret);
-//        appleSoft.getEmployees().add(paulaWalk);
-//        appleSoft.getEmployees().add(donaldBret);
-//
-//        paulaWalk.getCompanies().add(softwareApp);
-//        paulaWalk.getCompanies().add(appleSoft);
-//        johnStaff.getCompanies().add(softwareLen);
-//        donaldBret.getCompanies().add(appleSoft);
-//        donaldBret.getCompanies().add(softwareLen);
-//
-//        companyDao.save(softwareApp);
-//        companyDao.save(softwareLen);
-//        companyDao.save(appleSoft);
-//
-//        //When
-//        List<Employee> employeeSearchFacade = manyToManyFacade.findEmployeeLike("Walk");
-//        //Then
-//        Assert.assertEquals(1, employeeSearchFacade.size());
-//        //CleanUp
-//        try {
-//            companyDao.delete(softwareApp);
-//            companyDao.delete(softwareLen);
-//            companyDao.delete(appleSoft);
-//        } catch (Exception e) {
-//            //do nothing
-//        }
-//    }
-//
-//    @Test
-//    public void findCompanyNameLike() {
-//        //Given
-//        //Given
-//        Employee paulaWalk = new Employee("Paul", "Walk");
-//        Employee johnStaff = new Employee("John", "Staff");
-//        Employee donaldBret = new Employee("Donald", "Bret");
-//
-//        Company softwareApp = new Company("Software App");
-//        Company softwareLen = new Company("Software len");
-//        Company appleSoft = new Company("Apple Soft");
-//
-//        softwareApp.getEmployees().add(paulaWalk);
-//        softwareLen.getEmployees().add(johnStaff);
-//        softwareLen.getEmployees().add(donaldBret);
-//        appleSoft.getEmployees().add(paulaWalk);
-//        appleSoft.getEmployees().add(donaldBret);
-//
-//        paulaWalk.getCompanies().add(softwareApp);
-//        paulaWalk.getCompanies().add(appleSoft);
-//        johnStaff.getCompanies().add(softwareLen);
-//        donaldBret.getCompanies().add(appleSoft);
-//        donaldBret.getCompanies().add(softwareLen);
-//
-//        companyDao.save(softwareApp);
-//        companyDao.save(softwareLen);
-//        companyDao.save(appleSoft);
-//
-//        //When
-//        List<Company> companySearchFacade = manyToManyFacade.findCompanyLike("Sof");
-//        //Then
-//        Assert.assertEquals(2, companySearchFacade.size());
-//    }
-//}
+package com.kodilla.hibernate.manytomany.facade;
+
+import com.kodilla.hibernate.manytomany.Company;
+import com.kodilla.hibernate.manytomany.Employee;
+import com.kodilla.hibernate.manytomany.dao.CompanyDao;
+import com.kodilla.hibernate.manytomany.dao.EmployeeDao;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class ManyToManyFacadeTest {
+    @Autowired
+    private ManyToManyFacade manyToManyFacade;
+    @Autowired
+    private CompanyDao companyDao;
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @Test
+    public void findEmployeeNameLikeTest() {
+        //Given
+        Employee paulaWalk = new Employee("Paul", "Walk");
+        Employee johnStaff = new Employee("John", "Staff");
+        Employee donaldBret = new Employee("Donald", "Bret");
+
+        Company softwareApp = new Company("Software App");
+        Company softwareLen = new Company("Software len");
+        Company appleSoft = new Company("Apple Soft");
+
+        softwareApp.getEmployees().add(paulaWalk);
+        softwareLen.getEmployees().add(johnStaff);
+        softwareLen.getEmployees().add(donaldBret);
+        appleSoft.getEmployees().add(paulaWalk);
+        appleSoft.getEmployees().add(donaldBret);
+
+        paulaWalk.getCompanies().add(softwareApp);
+        paulaWalk.getCompanies().add(appleSoft);
+        johnStaff.getCompanies().add(softwareLen);
+        donaldBret.getCompanies().add(appleSoft);
+        donaldBret.getCompanies().add(softwareLen);
+
+        companyDao.save(softwareApp);
+        companyDao.save(softwareLen);
+        companyDao.save(appleSoft);
+
+        //When
+        List<Employee> employeeSearchFacade = manyToManyFacade.findEmployeeLike("Walk");
+        //Then
+        Assert.assertEquals(1, employeeSearchFacade.size());
+        //CleanUp
+        try {
+            companyDao.delete(softwareApp);
+            companyDao.delete(softwareLen);
+            companyDao.delete(appleSoft);
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
+
+    @Test
+    public void findCompanyNameLike() {
+        //Given
+        //Given
+        Employee paulaWalk = new Employee("Paul", "Walk");
+        Employee johnStaff = new Employee("John", "Staff");
+        Employee donaldBret = new Employee("Donald", "Bret");
+
+        Company softwareApp = new Company("Software App");
+        Company softwareLen = new Company("Software len");
+        Company appleSoft = new Company("Apple Soft");
+
+        softwareApp.getEmployees().add(paulaWalk);
+        softwareLen.getEmployees().add(johnStaff);
+        softwareLen.getEmployees().add(donaldBret);
+        appleSoft.getEmployees().add(paulaWalk);
+        appleSoft.getEmployees().add(donaldBret);
+
+        paulaWalk.getCompanies().add(softwareApp);
+        paulaWalk.getCompanies().add(appleSoft);
+        johnStaff.getCompanies().add(softwareLen);
+        donaldBret.getCompanies().add(appleSoft);
+        donaldBret.getCompanies().add(softwareLen);
+
+        companyDao.save(softwareApp);
+        companyDao.save(softwareLen);
+        companyDao.save(appleSoft);
+
+        //When
+        List<Company> companySearchFacade = manyToManyFacade.findCompanyLike("Sof");
+        //Then
+        Assert.assertEquals(2, companySearchFacade.size());
+    }
+}
